6f0627b65dec4b52d97384dd3c906f1949556783
Miqzo <mvainamo@gmail.com>
2025-08-20 18:19:37 +0300

    Initialized the solution skeleton for the app.

---
+++ RestaurantRater.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantRater.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RestaurantRater.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantRater.Maui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
