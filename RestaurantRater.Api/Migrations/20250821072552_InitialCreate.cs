d21979e5c4bce21e46dd0a5fda7258df7bea16f9
Miqzo <mvainamo@gmail.com>
2025-08-21 10:29:22 +0300

    Implemented database integration and data layers

---
+++ RestaurantRater.Api/Migrations/20250821072552_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantRater.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    Location = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    PriceBracket = table.Column<int>(type: "INTEGER", nullable: false),
+                    Style = table.Column<int>(type: "INTEGER", nullable: false),
+                    FoodScore = table.Column<int>(type: "INTEGER", nullable: false),
+                    DrinksScore = table.Column<int>(type: "INTEGER", nullable: false),
+                    DessertScore = table.Column<int>(type: "INTEGER", nullable: false),
+                    SettingScore = table.Column<int>(type: "INTEGER", nullable: false),
+                    ValueScore = table.Column<int>(type: "INTEGER", nullable: false),
+                    Review = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    ChildFriendlyScore = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                    table.CheckConstraint("CK_Restaurant_PriceBracket", "PriceBracket BETWEEN 1 AND 5");
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
--- RestaurantRater.Api/Program.cs
+++ RestaurantRater.Api/Program.cs
@@ -1,9 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantRater.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<RestaurantContext>(options =>
+    {
+        var cs = builder.Configuration.GetConnectionString("Default");
+        options.UseSqlite(cs);
+    });
+
+//Add controllers and Swagger
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
